33524ef283b295084444a618e6dd3ebaa5d0ff6d
Edo Bagus <ebagus409@gmail.com>
2024-06-04 12:42:11 +0700

    Connect to local database

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
+COUNT_BOOK = """SELECT COUNT(*) AS total_rows FROM public."Book";"""
+SELECT_BOOK_BY_ID = """SELECT "BookName" FROM public."Book" WHERE "BookID" = %s;"""
 
 app = Flask(__name__)
+url = "postgresql://postgres:Rikoremot1@localhost:5432/postgres"
+connection = psycopg2.connect(url)
 
-@app.get('/')
-def home():
-    return 'hello world su'
+@app.get('/api/book_count')
+def get_book_count():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(COUNT_BOOK)
+            book_count = cursor.fetchone()[0]
+    return jsonify({'book_count': book_count})
+
+@app.get('/api/book_name')
+def get_book_name():
+    book_id = request.args.get('id')
+    if not book_id:
+        return jsonify({'error': 'Book ID is required'}), 400
+    
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_BOOK_BY_ID, (book_id,))
+            result = cursor.fetchone()
+            if result:
+                book_name = result[0]
+                return jsonify({'book_id': book_id, 'book_name': book_name})
+            else:
+                return jsonify({'error': 'Book not found'}), 404
